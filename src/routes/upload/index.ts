f5a51f726cf2aef3806b2ea55a8642adc411be39
Connor Elliott <connorielliott01@gmail.com>
2023-04-24 16:27:23 -0500

    style: :art: Reformat code

--- src/routes/upload/index.ts
+++ src/routes/upload/index.ts
@@ -10,38 +10,45 @@ const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
 router.post("/", upload.single("file"), async (req: Request, res: Response) => {
-	try {
-		// Get the uploaded file
-		const filePath = req.file.path;
+  try {
+    // Get the uploaded file
+    const filePath = req.file.path;
 
-		// Unzip the file using AdmZip
-		const zip = new AdmZip(filePath);
-		const originalNameWithoutExt = path.basename(req.file.originalname, path.extname(req.file.originalname));
-		const outputFolderPath = `./uploads/${originalNameWithoutExt}`;
-		zip.extractAllTo('./uploads', true);
+    // Unzip the file using AdmZip
+    const zip = new AdmZip(filePath);
+    const originalNameWithoutExt = path.basename(
+      req.file.originalname,
+      path.extname(req.file.originalname)
+    );
+    const outputFolderPath = `./uploads/${originalNameWithoutExt}`;
+    zip.extractAllTo("./uploads", true);
 
-		console.log("Extraction complete!");
+    console.log("Extraction complete!");
 
-		// Run the PowerSynth script
-		const exitCode = await runPowerSynthScript(`${outputFolderPath}/macro_script.txt`);
+    // Run the PowerSynth script
+    const exitCode = await runPowerSynthScript(
+      `${outputFolderPath}/macro_script.txt`
+    );
 
-		// Delete the temporary file
-		fs.unlink(filePath, (err) => {
-			if (err) {
-				console.error("Error deleting file:", err);
-			}
-		});
+    // Delete the temporary file
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error("Error deleting file:", err);
+      }
+    });
 
-		if (exitCode === 0) {
-			// Send a success response
-			res.status(200).json({ message: "File unzipped, saved, and script executed" });
-		} else {
-			res.status(500).json({ message: "Internal server error" });
-		}
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: "Internal server error" });
-	}
+    if (exitCode === 0) {
+      // Send a success response
+      res
+        .status(200)
+        .json({ message: "File unzipped, saved, and script executed" });
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 export default router;
